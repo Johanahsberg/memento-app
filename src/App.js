06bbaacb13a14db7080f5898959b3349521d8880
Johan Åhsberg <johan.ahsberg@knowit.se>
2022-10-17 10:32:45 +0200

    Initial Setups from first video

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import shuffle from "./utilities/shuffle";
+import Card from "./components/Card";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [cards, setCards] = useState(shuffle);
+
+	return (
+		<div className='grid'>
+			{shuffle.map((card) => {
+				const { image, id, matched } = card;
+
+				return (
+					<Card key={id} image={image} selected={false} onClick={() => {}} />
+				);
+			})}
+		</div>
+	);
 }
 
 export default App;
